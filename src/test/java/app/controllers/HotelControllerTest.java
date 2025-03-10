ce65f369fe0dea8cbad4b7a50027a5fb5c23cd9d
Peter Bollhorn <pbollhorn@gmail.com>
2025-03-10 14:52:45 +0100

    Test can now run

--- src/test/java/app/controllers/HotelControllerTest.java
+++ src/test/java/app/controllers/HotelControllerTest.java
@@ -1,6 +1,12 @@
 package app.controllers;
 
+import app.config.HibernateConfig;
+import app.daos.HotelDao;
+import app.daos.RoomDao;
+import app.entities.Hotel;
+import app.entities.Room;
 import io.restassured.RestAssured;
+import jakarta.persistence.EntityManagerFactory;
 import org.junit.jupiter.api.*;
 
 import static io.restassured.RestAssured.given;
@@ -13,11 +19,32 @@ public class HotelControllerTest
     @BeforeAll
     static void setupAll()
     {
+        EntityManagerFactory emf = HibernateConfig.getEntityManagerFactoryForTest();
+        HotelDao hotelDao = HotelDao.getInstance(emf);
+        RoomDao roomDao = RoomDao.getInstance(emf);
+
+        Hotel h1 = new Hotel("Mågevejens Hotel", "Mågevej 1, 2400 København NV");
+        h1.addRoom(new Room("101", 1000.0));
+        h1.addRoom(new Room("102", 1000.0));
+        h1.addRoom(new Room("103", 1000.0));
+        h1.addRoom(new Room("201", 1500.0));
+        h1.addRoom(new Room("202", 1500.0));
+        hotelDao.create(h1);
+
+        Hotel h2 = new Hotel("Byens Hotel", "Bjergbygade 1, 4200 Slagelse");
+        h2.addRoom(new Room("1A", 600.0));
+        h2.addRoom(new Room("1B", 600.0));
+        h2.addRoom(new Room("1C", 600.0));
+        h2.addRoom(new Room("2A", 1800.0));
+        hotelDao.create(h2);
+
         ApplicationConfig
                 .getInstance()
                 .initiateServer()
                 .setRoute(Routes.getRoutes())
+                .handleException()
                 .startServer(7777);
+
         RestAssured.baseURI = "http://localhost:7777/api";
     }
 
